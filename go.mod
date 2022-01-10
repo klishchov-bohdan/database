c956d3f894155d2151526855c8969d981fa9f601
klishchov-bohdan <bogdan.bogdan2525@gmail.com>
2022-01-10 16:27:20 +0200

    test db working

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/klishchov-bohdan/database
+
+go 1.13
+
+require (
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.4
+)
--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
 
+type Book struct {
+	ID          int
+	Title       string
+	Description string
+	PageCount   int
+	PubYear     int
+}
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "bohdan:Bohdan.2525@tcp(127.0.0.1:3306)/shop?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDB.Close()
+
+	sqlDB.SetMaxIdleConns(2)
+	sqlDB.SetMaxOpenConns(5)
+	sqlDB.SetConnMaxLifetime(2 * time.Second)
+
+	book := &Book{}
+	res := db.First(book, 3)
+	if res.Error != nil {
+		log.Fatal(res.Error)
+	}
+	fmt.Print(book)
 }
